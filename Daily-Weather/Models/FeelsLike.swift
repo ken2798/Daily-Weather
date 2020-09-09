643c30fb89a944d23cf8be98a707b5f9f07b1d39
ken2798 <44506559+ken2798@users.noreply.github.com>
2020-09-09 10:24:07 +0700

    add models

---
+++ Daily-Weather/Models/FeelsLike.swift
@@ -0,0 +1,34 @@
+//
+//  FeelsLike.swift
+//  Daily-Weather
+//
+//  Created by Nguyen Tien Cong on 8/17/20.
+//  Copyright © 2020 Nguyen Tien Cong. All rights reserved.
+//
+
+import Foundation
+
+struct FeelsLike  {
+    var day: Float
+    var night: Float
+    var evening: Float
+    var morning: Float
+  
+    enum FeelsLike: String, CodingKey {
+           case day = "day"
+           case night = "night"
+           case evening = "eve"
+           case morning = "morn"
+
+       }
+    }
+
+extension FeelsLike: Decodable {
+   init(from decoder: Decoder) throws {
+       let feelsLikeContainer = try decoder.container(keyedBy: FeelsLike.self)
+       day = try feelsLikeContainer.decode(Float.self, forKey: .day)
+       night = try feelsLikeContainer.decode(Float.self, forKey: .night)
+       evening = try feelsLikeContainer.decode(Float.self, forKey: .evening)
+       morning = try feelsLikeContainer.decode(Float.self, forKey: .morning)
+   }
+}
